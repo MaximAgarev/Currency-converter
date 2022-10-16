ed9d862f1474bc09f22788332a9539217ce1ed98
MaximAgarev <maximagarev@yandex.ru>
2022-10-16 21:48:45 +0400

    Outlets described

--- Currency converter/ViewController.swift
+++ Currency converter/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amdLabel: UILabel!
+    @IBOutlet weak var rubLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var eurLabel: UILabel!
+    @IBOutlet weak var aedLabel: UILabel!
+    @IBOutlet weak var tklLabel: UILabel!
+    
+    @IBOutlet weak var amdValue: UITextField!
+    @IBOutlet weak var rubValue: UITextField!
+    @IBOutlet weak var usdValue: UITextField!
+    @IBOutlet weak var eurValue: UITextField!
+    @IBOutlet weak var aedValue: UITextField!
+    @IBOutlet weak var tklValue: UITextField!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var updateLabel: UILabel!
+    @IBOutlet weak var updateSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
