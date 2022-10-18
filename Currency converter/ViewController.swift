c3a4f61d4da8337bd3527f00b06946ebd5fa45df
MaximAgarev <maximagarev@yandex.ru>
2022-10-19 01:01:33 +0400

    All fields being calculated in cycle

--- Currency converter/ViewController.swift
+++ Currency converter/ViewController.swift
@@ -13,12 +13,12 @@ private extension String {
     }
 }
 
+private var rubRate: Float = 1
 private var amdRate: Float = 0.1561
 private var usdRate: Float = 63.06
 private var eurRate: Float = 62.47
 private var aedRate: Float = 16.91
 private var tklRate: Float = 3.39
-
 
 class ViewController: UIViewController {
 
@@ -43,6 +43,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var aedValue: UITextField!
     @IBOutlet weak var tklValue: UITextField!
     
+    var allValueFields: [UITextField] = []
+    
+    func takeRate(field: UITextField) -> Float {
+
+        let fieldSelected = field
+
+         switch fieldSelected{
+         case amdValue:
+             return amdRate
+         case usdValue:
+             return usdRate
+         case eurValue:
+             return eurRate
+         case aedValue:
+             return aedRate
+         case tklValue:
+             return tklRate
+         case rubValue:
+             return rubRate
+         default:
+             return 1
+         }
+    }
+
+    
     @IBOutlet weak var resetButton: UIButton!
     
     @IBOutlet weak var updateLabel: UILabel!
@@ -58,16 +83,74 @@ class ViewController: UIViewController {
         aedRateLabel.text = String((aedRate))
         tklRateLabel.text = String((tklRate))
         
+        allValueFields.append(rubValue)
+        allValueFields.append(amdValue)
+        allValueFields.append(usdValue)
+        allValueFields.append(eurValue)
+        allValueFields.append(aedValue)
+        allValueFields.append(tklValue)
+        
         amdValue.becomeFirstResponder()
+        amdValue.backgroundColor = .systemGray6
     }
 
-
+    private func clearFields() {
+        for i in allValueFields {
+            i.text = ""
+        }
+    }
+    
+    private func colorizeFields(color: UIColor) {
+        for i in allValueFields {
+            i.backgroundColor = color
+        }
+    }
+    
+    private func colorizeOnTouchDown(field: UITextField) {
+        colorizeFields(color: .clear)
+        field.backgroundColor = .systemGray6
+    }
+    
+    
+    private func calculateValues(field: UITextField) {
+        for i in allValueFields {
+            if i != field {
+                if i == rubValue {
+                    i.text = String(Int(round(field.text!.floatValue * takeRate(field: field))))
+                } else {
+                    i.text = String(Int(round(field.text!.floatValue * takeRate(field: field) / takeRate(field: i))))
+                }
+                
+            }
+        }
+    }
+    
+    @IBAction func updateDidSwitch(_ sender: Any) {
+        if updateSwitch.isOn {
+            clearFields()
+            colorizeFields(color: .systemGray6)
+        } else {
+            clearFields()
+            colorizeFields(color: .clear)
+        }
+    }
+    
+    @IBAction func amdDidTouchDown(_ sender: Any) {
+        colorizeOnTouchDown(field: amdValue)
+    }
+    
     @IBAction func amdDidChange(_ sender: Any) {
-        rubValue.text = String(Int(round(amdValue.text!.floatValue * amdRate)))
+//        rubValue.text = String(Int(round(amdValue.text!.floatValue * amdRate)))
+        calculateValues(field: amdValue)
+    }
+    
+    
+    @IBAction func rubDidTouchDown(_ sender: Any) {
+        colorizeOnTouchDown(field: rubValue)
     }
     
     @IBAction func rubDidChange(_ sender: Any) {
-        amdValue.text = String(Int(round(rubValue.text!.floatValue * amdRate)))
+        amdValue.text = String(Int(round(rubValue.text!.floatValue / amdRate)))
     }
         
     @IBAction func resetDidTap(_ sender: Any) {
